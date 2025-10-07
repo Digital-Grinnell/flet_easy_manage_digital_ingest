a30cdbd820a44c674b1c1c552b8c0dc891d78dd0
SummittDweller <mark.mcfate@icloud.com>
2025-10-06 23:30:59 -0500

    MacOS entitlements still not working, use 'flet run --web'

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from pathlib import Path
+import warnings
 
 import flet_easy as fs
 from core.config import ConfigApp
 
+# We ignore deprecation warnings from Flet.  These are due to Flet-Easy using some deprecated Flet features
+# that have not yet been updated in Flet-Easy.  These warnings will be removed in future releases of Flet-Easy.
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# We create the Flet-Easy application instance
 app = fs.FletEasy(
     route_init="/home",
     path_views=Path(__file__).parent / "views"
